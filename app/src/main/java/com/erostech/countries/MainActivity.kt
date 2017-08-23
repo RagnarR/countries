f0bec7b53226f2ce38aa6c4aeaebce0d091c9a0b
eros902002@googlemail.com <eros902002@googlemail.com>
2017-08-23 17:19:52 +0200

    Initial commit

---
+++ app/src/main/java/com/erostech/countries/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.erostech.countries
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        Fabric.with(this, Crashlytics())
+    }
+}
